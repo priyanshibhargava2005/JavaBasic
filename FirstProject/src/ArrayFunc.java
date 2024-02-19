2e2d31c821e278a13056897091b4f7a58225d8e1
priyanshibhargava2005 <priyanshi.bhargava05@gmail.com>
2024-02-19 09:37:41 +0530

    Addede some codes with array

---
+++ FirstProject/src/ArrayFunc.java
@@ -0,0 +1,36 @@
+//print in reverse
+//print every alternate number from array
+//print all even numbers from 0 to 100
+
+//code
+public class ArrayFunc {
+    
+    public static void main(String[] args) {
+        // Sample code provided
+        int[] array1 = new int[10];
+        int[] array2 = new int[10];
+        int[] array3 = {1, 2, 3, 4, 5};
+                
+        String[] words = {"Hello", "World"}; //string array
+        
+        // Reversed string array
+        System.out.println("Reversed String Array:");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.println(words[i]);
+        }
+        
+        // Print every alternate number from array
+        System.out.println("Every alternate number from array3:");
+        for (int i = 0; i < array3.length; i += 2) {
+            System.out.println(array3[i]);
+        }
+        
+     // Print all even numbers from 0 to 100
+        System.out.println("Even numbers from 0 to 100:");
+        for (int i = 0; i <= 100; i ++) {
+        	if (i%2==0) {
+            System.out.println(i);
+        	}
+        }
+    }
+}
