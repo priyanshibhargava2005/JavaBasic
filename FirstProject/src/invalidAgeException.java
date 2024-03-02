f046de54c08e58d27567f17b5fa382aad0f4bef9
priyanshibhargava2005 <priyanshi.bhargava05@gmail.com>
2024-03-02 12:00:11 +0530

    made changes

--- FirstProject/src/invalidAgeException.java
+++ FirstProject/src/invalidAgeException.java
@@ -1,38 +1,36 @@
 import java.util.Scanner;
 
-public class invalidAgeExceptionHandler{
-	public static void main(String[]args) {
+public class invalidAgeException {
+	public static void main(String[] args) {
 		System.out.println("Enter your age");
-		Scanner scanner=new Scanner(System.in);
-		int a=scanner.nextInt();
-	
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+
 		try {
-			checkAgeHandler(age);
-		}
-		catch(invalidAgeException e) {
-//			System.out.println(e.getmessage());
+			checkAgeHandler(a);
+		} catch (invalidAgeException e) {
+			// System.out.println(e.getmessage());
 			e.printStackTrace();
 		}
 	}
 
+	static void checkAgeHandler(int a) throws invalidAgeException {
+		checkAge(a);
+	}
 
-static void checkAgeHandler(int a) throws invalidAgeException{
-	checkAge(age);
-}
+	static void checkAge(int a) throws invalidAgeException {
+		if (a < 18) {
+			throw new invalidAgeException("You are not eligible as you are not an adult");
+		} else {
+			System.out.println("Welcome");
+		}
+	}
 
-static void checkAge(int a) throws invalidAgeException{
-	if(a<18) {
-		throw new invalidAgeException("You are not eligible as you are not an adult");
-	}else {
-		System.out.println("Welcome");
-	}	
-}
-
-class invalidAgeException extends Exception{
+	class invalidAgeException extends Exception {
 	public invalidAgeException(String message) {
+
 		public invalidAgeException(String message) {
 			super(message);
 		}
 	}
 }
-
